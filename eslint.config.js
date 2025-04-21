ce87cfb957f28792b44861d07a76d9136cb4341c
ouadiesoft@gmail.comt <Ouadie Lahdioui>
2025-04-21 23:34:33 +0200

    lint: Add eslint config file

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off'
+    }
+  }
+];
